d867aba6170ec49e7772e83fe56b4dacf4c3ed40
Arvind <arvindkasale@gmail.com>
2016-09-08 23:48:36 +0530

    Added reverse geocode to new

--- src/main/webapp/resources/client/config/config.js
+++ src/main/webapp/resources/client/config/config.js
@@ -7,7 +7,7 @@
 		cfg.maps = {
 			/*Default to NewYork*/
 			defaultConfig : { center: { latitude: 40.710496, longitude: -74.006333 }, zoom: 15 }, /*Default to NewYork*/
-			defaultMarker : {iconImage : "", id: "myLoc",coords: {latitude: 40.710496,longitude: -74.006333},options: {icon: "/store-locator/web-resources/static/images/icon_store.jpg", draggable: true, clickable : true }}
+			defaultMarker : {address : "9/11 Tribute Center, 120 Liberty Street New York, NY 10006, USA", id: "myLoc",coords: {latitude: 40.710496,longitude: -74.006333},options: {icon: "/store-locator/web-resources/static/images/shop_48.png", draggable: true, clickable : true, title : "Current Location" }}
 		};
 		
 		return cfg;
--- src/main/webapp/resources/client/controllers/vmStoresCtrl.js
+++ src/main/webapp/resources/client/controllers/vmStoresCtrl.js
@@ -6,10 +6,40 @@
 		var vm = this;
 		vm.map = ConfigService.maps.defaultConfig;
 		vm.marker = ConfigService.maps.defaultMarker;
+		vm.address = ConfigService.maps.defaultMarker.address;
+		vm.lat_lon = ConfigService.maps.defaultMarker.coords.latitude+","+ConfigService.maps.defaultMarker.coords.longitude;
+		var geocoder = null;
+		/*Add events to marker*/
+		vm.marker.events = {
+		        dragend: function (marker, eventName, args) {
+		            var lat = marker.getPosition().lat();
+		            var lon = marker.getPosition().lng();
+		            vm.lat_lon = lat+","+lon;
+		            geocoder.geocode({'location': marker.getPosition()}, function(results, status) {
+		            	if (status === 'OK') {
+		                  if (results[1]) {
+		                    vm.address = results[0].formatted_address;
+		                  } else {
+		                    window.alert('No results found');
+		                  }
+		                } else {
+		                  window.alert('Geocoder failed due to: ' + status);
+		                }
+		            });
+		          }
+		};
+		
+		/* Update marker 
+		vm.marker.options.labelContent = "lat: " + vm.marker.coords.latitude + ' ' + 'lon: ' + vm.marker.coords.longitude;
+		vm.marker.options.labelAnchor = "100 0";
+		vm.marker.options.labelClass = "marker-labels";
+		*/
+		console.log(vm.marker);
 		
 		//uiGmapGoogleMapApi promise when resolved we need to add vm.map data
 		uiGmapGoogleMapApi.then(function(maps){
-			console.log(maps);
+			geocoder = new google.maps.Geocoder();
+			console.log(geocoder);
 		})
 	};
 	
